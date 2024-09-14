29a21ab82c3f1e26e131fe0f347264d9e7940ec8
Krishna <kvrmoorthi.93@gmail.com>
2024-09-14 13:37:58 +0530

    Commit 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,22 @@
 import streamlit as st
+import pandas as pd
 
-st.title('🎈 App Name')
+st.title('🧠 BrainStroke Prediction Machine learning Model 🤖')
 
-st.write('Hello world!')
+st.info('This app predicts the occurance of Brain Strokes based on your inputs')
+
+with st.expander('Data'):
+    st.write('**Raw Data**')
+    df = pd.read_csv('https://raw.githubusercontent.com/arpitgour16/Brain_Stroke_prediction_analysis/main/healthcare-dataset-stroke-data.csv')
+    df
+
+    st.write('**X**')
+    X = df.drop({'stroke','id'},axis=1)
+    X
+    
+    st.write('**y**')
+    y = df.stroke
+    y
+             
+with st.expander('Data isualization'):
+    st.scatter_chart(data=df, x=)
